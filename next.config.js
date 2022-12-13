0acc0285fcab4784025798239f321f50dd1a7223
Enoch Fabiyi <ayomikun204@gmail.com>
2022-12-13 22:12:49 +0100

    next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  webpack: (config, options) => {
+    const { isServer } = options;
+    config.module.rules.push({
+      test: /\.(ogg|mp3|wav|mpeg|png|jpeg|gif|svg)$/i,
+      exclude: config.exclude,
+      use: [
+        {
+          loader: require.resolve("file-loader"),
+          options: {
+            limit: config.inlineImageLimit,
+            publicPath: `_next/static/images/`,
+            outputPath: `${isServer ? "../" : ""}static/images/`,
+            name: "[name]-[hash].[ext]",
+            esModule: config.esModule || false,
+          },
+        },
+      ],
+    });
+    return config;
+  },
+  reactStrictMode: false,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
